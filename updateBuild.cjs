0e5b77ba5c0bfcba3ac90498f00a6bf393d645f5
jam54 <git@jam54.com>
2023-05-21 19:59:52 +0200

    We now keep track of a build number which gets incremented every time either the `npm run tauri dev` or `npm run tauri build` command is run

---
+++ updateBuild.cjs
@@ -0,0 +1,55 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+const packageJsonPath = path.join(__dirname, 'package.json');
+const tauriConfFilePath = path.join(__dirname, 'src-tauri', 'tauri.conf.json');
+
+// Read package.json to get name, version, and build
+fs.readJson(packageJsonPath, (err, packageData) =>
+{
+    if (err)
+    {
+        console.error('Error reading package.json:', err);
+        return;
+    }
+
+    const {name, version, build} = packageData;
+    const updatedBuildNumber = build + 1;
+
+    // Update the buildNumber in package.json
+    fs.writeJson(packageJsonPath, {...packageData, build: updatedBuildNumber}, {spaces: 4}, writeErr =>
+    {
+        if (writeErr)
+        {
+            console.error('Error updating package.json:', writeErr);
+            return;
+        }
+
+        console.log('buildNumber updated in package.json');
+
+        // Update tauri.conf.json
+        fs.readJson(tauriConfFilePath, (tauriConfReadErr, tauriConfData) =>
+        {
+            if (tauriConfReadErr)
+            {
+                console.error('Error reading tauri.conf.json:', tauriConfReadErr);
+                return;
+            }
+
+            // Update the windows.title property
+            tauriConfData.tauri.windows[0].title = `${name} ${version} (${updatedBuildNumber})`;
+
+            // Write the updated tauri.conf.json
+            fs.writeJson(tauriConfFilePath, tauriConfData, {spaces: 4}, writeTauriConfErr =>
+            {
+                if (writeTauriConfErr)
+                {
+                    console.error('Error updating tauri.conf.json:', writeTauriConfErr);
+                    return;
+                }
+
+                console.log(`tauri.conf.json updated with tauri.windows.title: ${tauriConfData.tauri.windows[0].title}`);
+            });
+        });
+    });
+});
