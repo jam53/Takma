43f6f89d7a88ac2538fd77040f28763c34b2da99
jam54 <git@jam54.com>
2023-08-04 19:45:00 +0200

    Todo items in a checklist can now be dragged and dropped

--- src/components/BoardScreen/CheckList.svelte
+++ src/components/BoardScreen/CheckList.svelte
@@ -4,6 +4,7 @@
     import {tweened} from "svelte/motion";
     import {cubicOut} from "svelte/easing";
     import {slide} from "svelte/transition";
+    import {dndzone} from "svelte-dnd-action";
 
     export let cardToSave;
     export let setTypingFunction;
@@ -94,8 +95,8 @@
             </span>
             <progress id={`checklist-progress-${checklist.id}`} value={tweenProgressBarValue(checklist.id, amountOfTodosInChecklistFunction(checklist, true) / amountOfTodosInChecklistFunction(checklist))}/>
         </div>
-        <div class="todosHolder">
-            {#each checklist.todos as todo, j}
+        <div class="todosHolder" use:dndzone={{items: checklist.todos, type:"todo", dropTargetStyle: {}, zoneTabIndex: -1}} on:consider={e => checklist.todos = e.detail.items} on:finalize={e => checklist.todos = e.detail.items}>
+            {#each checklist.todos as todo, j (todo.id)}
                 <div class="todoContainer"
                      in:slide|global={{duration: 0}}
                      on:introend={() => {
@@ -139,7 +140,8 @@
     <!--This checks if the enter keys was pressed and if it was, it unfocuses the span, so we no longer edit the content of it-->
                         {SaveLoadManager.getData().getCard($selectedBoardId, $selectedCardId)?.checklists[i].todos[j].content}
                     </span>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                    <svg stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M5.5 4.625C6.12132 4.625 6.625 4.12132 6.625 3.5C6.625 2.87868 6.12132 2.375 5.5 2.375C4.87868 2.375 4.375 2.87868 4.375 3.5C4.375 4.12132 4.87868 4.625 5.5 4.625ZM9.5 4.625C10.1213 4.625 10.625 4.12132 10.625 3.5C10.625 2.87868 10.1213 2.375 9.5 2.375C8.87868 2.375 8.375 2.87868 8.375 3.5C8.375 4.12132 8.87868 4.625 9.5 4.625ZM10.625 7.5C10.625 8.12132 10.1213 8.625 9.5 8.625C8.87868 8.625 8.375 8.12132 8.375 7.5C8.375 6.87868 8.87868 6.375 9.5 6.375C10.1213 6.375 10.625 6.87868 10.625 7.5ZM5.5 8.625C6.12132 8.625 6.625 8.12132 6.625 7.5C6.625 6.87868 6.12132 6.375 5.5 6.375C4.87868 6.375 4.375 6.87868 4.375 7.5C4.375 8.12132 4.87868 8.625 5.5 8.625ZM10.625 11.5C10.625 12.1213 10.1213 12.625 9.5 12.625C8.87868 12.625 8.375 12.1213 8.375 11.5C8.375 10.8787 8.87868 10.375 9.5 10.375C10.1213 10.375 10.625 10.8787 10.625 11.5ZM5.5 12.625C6.12132 12.625 6.625 12.1213 6.625 11.5C6.625 10.8787 6.12132 10.375 5.5 10.375C4.87868 10.375 4.375 10.8787 4.375 11.5C4.375 12.1213 4.87868 12.625 5.5 12.625Z" fill="currentColor"></path></svg>
+                    <svg class="deleteTodoButton" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
                          on:click={() => checklist.todos = checklist.todos.filter(todoo => todoo.id !== todo.id)}
                     >
                         <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clip-rule="evenodd" />
@@ -319,11 +321,11 @@
         min-width: 1.25em;
         max-width: 1.25em;
         color: var(--unselected-button);
-        cursor: pointer;
         transition: 0.3s;
     }
 
-    .todoContainer svg:hover {
+    .deleteTodoButton:hover {
+        cursor: pointer;
         color: var(--danger)
     }
 
