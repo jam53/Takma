a41b0f99778009f4c3a4aedffc0764cef4b1202a
jam54 <git@jam54.com>
2024-09-27 10:13:04 +0200

    Replace `<span>` tag with `<input>` for todo items in checklists

--- src/components/BoardScreen/CheckLists.svelte
+++ src/components/BoardScreen/CheckLists.svelte
@@ -51,7 +51,7 @@
     export function addTodoItem(checklist)
     {
         const newTodoId = crypto.randomUUID();
-        checklist.todos.push({id: newTodoId, completed: false, content: " "}); //Als we ipv " ", "" zouden setten als content. Dan hebben we het probleem dat wanneer we een todo toevoegen aan een lege checklist, elke toetsaanslag dubbel wordt geregistreert wanneer we typen om de titel/content van de todo aan te passen. Met een spatie " " als de content, hebben we geen last van dat probleem
+        checklist.todos.push({id: newTodoId, completed: false, content: ""});
 
         idAddTodoItemButtonToScrollTo = `addTodoButton-${checklist.id}`; //The refresh of the UI at the end of this function, will trigger the intro animation of the todoitems. And when that intro animation ends (on:introend) AKA once the todoitem is visible in the UI, the value of this variable will be used to scroll to the "add new todo item" button of the checklist
         idTodoItemToFocus = `todo-${newTodoId}`; //In the same way as above, this will be used to select the newly added todo item so it can be edited without having to click on it; once the todoitem's intro animation ends (=by using the introanimation to do this action we know that the elment exist in the DOM. Else we wouldn't be playing/finishing an intro animation. And we need to make sure the element exists in the DOM or else writing element.focus() in this function here would throw an error since the element of this todo isn't present in the DOM yet.
@@ -128,7 +128,6 @@
                          if (idTodoItemToFocus === `todo-${todo.id}`)
                          {
                              document.getElementById(`todo-${todo.id}`).focus(); //Focusing on the span makes it so it\'s being edited
-                             document.getElementById(`todo-${todo.id}`).dataset.newlyCreatedTodo = "true"; //We use this data attribute to know whether or not after pressing enter if we should create another todo item or not. Normally we just unfocus the span of the todo element on pressing enter. But when this is a todo element that was just created, it is more convenient that when we press enter, we create another new todo item. Which is done by checking if the data attribute is "true" or not.
                              idTodoItemToFocus = "";
                          }
                          if (idAddTodoItemButtonToScrollTo === `addTodoButton-${checklist.id}`)
@@ -146,29 +145,21 @@
                                 saveCardFunction();
                             }}
                     />
-                    <span role="textbox" contenteditable="plaintext-only"
+                    <input role="textbox" contenteditable="plaintext-only"
                           id={`todo-${todo.id}`}
                           style={`text-decoration: ${todo.completed ? "line-through" : "none"}`}
-                          on:input={(e) => todo.content = e.target.textContent}
+                          bind:value={todo.content}
                           on:focus={() => setTypingFunction(true)}
                           on:focusout={() => setTypingFunction(false)}
                           on:keydown={e => {
-                              if (e.keyCode === 13 && document.getElementById(`todo-${todo.id}`).dataset.newlyCreatedTodo === "true")
-                              {//This means we pressed the enter key on a todoitem that was just added. So here it is more convenient to create another new todoitem when pressing enter. Instead of just unfocussing it by e.target.blur() => unfocussing makes it so we are no longer editting it
+                              if (e.keyCode === 13)
+                              {// If Enter is pressed, create a new todo item
                                   e.preventDefault();
-                                  document.getElementById(`todo-${todo.id}`).dataset.newlyCreatedTodo = "false";
                                   addTodoItem(checklist);
-                              }
-                              else if (e.keyCode === 13)
-                              {
-                                  e.target.blur();
                               }
                           }}
                           on:keyup={() => waitUntilUserStoppedTyping(saveCardFunction)}
-                    >
-    <!--This checks if the enter keys was pressed and if it was, it unfocuses the span, so we no longer edit the content of it-->
-                        {SaveLoadManager.getData().getCard($selectedBoardId, $selectedCardId)?.checklists[i].todos[j].content}
-                    </span>
+                    />
                     <svg stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M5.5 4.625C6.12132 4.625 6.625 4.12132 6.625 3.5C6.625 2.87868 6.12132 2.375 5.5 2.375C4.87868 2.375 4.375 2.87868 4.375 3.5C4.375 4.12132 4.87868 4.625 5.5 4.625ZM9.5 4.625C10.1213 4.625 10.625 4.12132 10.625 3.5C10.625 2.87868 10.1213 2.375 9.5 2.375C8.87868 2.375 8.375 2.87868 8.375 3.5C8.375 4.12132 8.87868 4.625 9.5 4.625ZM10.625 7.5C10.625 8.12132 10.1213 8.625 9.5 8.625C8.87868 8.625 8.375 8.12132 8.375 7.5C8.375 6.87868 8.87868 6.375 9.5 6.375C10.1213 6.375 10.625 6.87868 10.625 7.5ZM5.5 8.625C6.12132 8.625 6.625 8.12132 6.625 7.5C6.625 6.87868 6.12132 6.375 5.5 6.375C4.87868 6.375 4.375 6.87868 4.375 7.5C4.375 8.12132 4.87868 8.625 5.5 8.625ZM10.625 11.5C10.625 12.1213 10.1213 12.625 9.5 12.625C8.87868 12.625 8.375 12.1213 8.375 11.5C8.375 10.8787 8.87868 10.375 9.5 10.375C10.1213 10.375 10.625 10.8787 10.625 11.5ZM5.5 12.625C6.12132 12.625 6.625 12.1213 6.625 11.5C6.625 10.8787 6.12132 10.375 5.5 10.375C4.87868 10.375 4.375 10.8787 4.375 11.5C4.375 12.1213 4.87868 12.625 5.5 12.625Z" fill="currentColor"></path></svg>
                     <svg class="deleteTodoButton" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
                          on:click={() => {
@@ -341,14 +332,15 @@
         align-content: space-around;
     }
 
-    .todoContainer span {
+    .todoContainer input[role="textbox"] {
         width: 100%;
-        cursor: pointer;
         transition: 0.2s;
+        border: none;
         border-radius: 0.25em;
         padding: 0.25em;
         word-break: break-word;
-        cursor: text;
+        background: transparent;
+        font-size: medium;
     }
 
     .todoContainer svg {
